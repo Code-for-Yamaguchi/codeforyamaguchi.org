4c2da47462d1dc55ceb5e1411a57aef4ecc257c4
nishidayoshikatsu <nsd24technology@gmail.com>
2021-04-05 23:13:39 +0900

    refactor: Running ESLint and Prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import '../styles/globals.scss'
-import {AppProps} from "next/app";
+import { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 
 export default function Home() {
-  return (
-      <></>
-  )
+  return <></>
 }
