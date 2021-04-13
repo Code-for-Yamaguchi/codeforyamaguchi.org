296a8f4aea306d4a22fc1c07f283c1000e9fa936
nishidayoshikatsu <nsd24technology@gmail.com>
2021-04-13 10:19:19 +0900

    feat: The header component mobile support(add modal code)

---
+++ src/components/MobileMenu.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+type Props = {
+    close: (e: any) => void;
+    children: React.ReactNode;
+};
+
+export const MobileMenu: React.FC<Props> = props => {
+    return (
+        <div onClick={props.close}>
+            <div>
+                {React.cloneElement(props.children as any, {
+                    close: props.close
+                })}
+            </div>
+        </div>
+    );
+};
