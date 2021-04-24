e64fd9ea265ca5a09eb6ebb7655d80100f2fea2f
nishidayoshikatsu <nsd24technology@gmail.com>
2021-04-25 02:48:23 +0900

    feat: Adding top news view

---
+++ src/components/LinkList.tsx
@@ -0,0 +1,25 @@
+import { NewsLinks } from '../data/news'
+
+export const LinkList = () => {
+    return (
+        <ul className="flex justify-center flex-col divide-y divide-gray-200 px-10 md:px-40 py-6">
+            {NewsLinks.map((newsLink) => (
+                <li key={newsLink.title} className="flex justify-start text-sm sm:text-xl">
+                    <a href={newsLink.url}>
+                        <div className="flex flex-row py-2">
+                            <div className="text-normal opacity-80 pr-4">
+                                {newsLink.date.toLocaleDateString('ja-JP')}
+                            </div>
+                            <div className="text-white items-center px-2.5 py-0.5 rounded-md text-sm font-medium bg-primary">
+                                {newsLink.tag}
+                            </div>
+                        </div>
+                        <div className="text-normal pb-2">
+                            {newsLink.title}
+                        </div>
+                    </a>
+                </li>
+            ))}
+        </ul>
+    )
+}
--- src/components/TopNews.tsx
+++ src/components/TopNews.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 
 import { SectionTitle } from './SectionTitle'
+import { LinkList } from './LinkList'
 
 export const TopNews = () => {
     return (
         <div>
             <SectionTitle title="News" subtitle="お知らせ" />
+            <LinkList />
         </div>
     )
 }
