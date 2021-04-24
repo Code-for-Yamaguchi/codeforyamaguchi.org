a225120370be216bd83c94fb0d8f70c737a51bdc
nishidayoshikatsu <nsd24technology@gmail.com>
2021-04-25 04:05:32 +0900

    style: Adjust section title component style

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -5,9 +5,9 @@ type Props = {
 
 export const SectionTitle = ({ title, subtitle }: Props) => {
     return (
-        <div className="border-l-8 border-green-500 mt-8 pl-20 px-4 py-6">
-            <div className="text-green-400 text-6xl mb-4">{title}</div>
-            <sub className="text-green-400 text-base">{subtitle}</sub>
+        <div className="border-l-8 border-green-500 mt-8 pl-9 md:pl-36 px-4 py-2">
+            <div className="text-primary text-3xl md:text-5xl mb-1">{title}</div>
+            <sub className="text-primary text-sm md:text-base">{subtitle}</sub>
         </div>
     )
 }
