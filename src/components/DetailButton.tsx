c4f17d1c6118b4037700dff7b0ba7eeda181ea4a
nishidayoshikatsu <nsd24technology@gmail.com>
2021-04-25 04:28:10 +0900

    feat: Adding DetailButton component

---
+++ src/components/DetailButton.tsx
@@ -0,0 +1,18 @@
+import { ChevronRightIcon } from '@heroicons/react/solid'
+
+import Link from 'next/link'
+
+type Props = {
+    link: string
+}
+
+export const DetailButton = ({ link }: Props ) => {
+    return (
+        <Link href={link}>
+            <div className="cursor-pointer flex justify-center text-normal text-primary border border-green-500 rounded-full py-4 px-10 mx-12 md:mx-40 hover:bg-primary hover:text-white">
+                もっと詳しく
+                <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+            </div>
+        </Link>
+    )
+}
--- src/components/TopNews.tsx
+++ src/components/TopNews.tsx
@@ -2,12 +2,14 @@ import Link from 'next/link'
 
 import { SectionTitle } from './SectionTitle'
 import { LinkList } from './LinkList'
+import { DetailButton } from './DetailButton'
 
 export const TopNews = () => {
     return (
         <div>
             <SectionTitle title="News" subtitle="お知らせ" />
             <LinkList />
+            <DetailButton link="/news" />
         </div>
     )
 }
