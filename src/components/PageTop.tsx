4abad47944fc16da1e40c807a1538fe35d99aab9
nishidayoshikatsu <nsd24technology@gmail.com>
2021-04-25 08:41:46 +0900

    feat: Adjust top title font-weight

--- src/components/PageTop.tsx
+++ src/components/PageTop.tsx
@@ -5,7 +5,7 @@ export const PageTop = () => {
   return (
     <div className="relative z-0">
       <div className="h-screen flex flex-col justify-center items-center px-4 py-6 sm:px-6 space-y-6">
-        <div className="flex justify-center text-gray-500 text-base sm:text-3xl clearfix">
+        <div className="flex justify-center text-gray-800 font-bold text-base sm:text-3xl clearfix">
           山口県の挑戦者が集まるコミュニティ
         </div>
         <div className="flex justify-center text-center text-gray-400 text-sm sm:text-xl clearfix">
