b24ea43fe82cedefb21e978e353ac64bd705d06e
nishidayoshikatsu <nsd24technology@gmail.com>
2021-05-11 02:55:59 +0900

    feat: Fix Detail Link

---
+++ src/pages/news/index.tsx
@@ -0,0 +1,11 @@
+import { PageHeader } from '../../components/PageHeader'
+import { PageFooter } from '../../components/PageFooter'
+
+export default function Home() {
+    return (
+        <div>
+            <PageHeader />
+            <PageFooter />
+        </div>
+    )
+}
