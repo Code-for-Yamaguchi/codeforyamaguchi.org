b965399f4a55a3f9ec79e25508c961551e82c6ae
nishida <42875682+nishidayoshikatsu@users.noreply.github.com>
2023-10-04 19:14:59 +0900

    :hammer: Set sky environment

--- src/components/fuku3D/index.tsx
+++ src/components/fuku3D/index.tsx
@@ -1,68 +1,33 @@
 'use client'
 
 import { Canvas, useFrame } from '@react-three/fiber'
-import { Environment, OrbitControls, useGLTF, Stage, BakeShadows, AccumulativeShadows, RandomizedLight, Center } from "@react-three/drei";
+import { Environment, OrbitControls, useGLTF, Stage, BakeShadows, AccumulativeShadows, RandomizedLight, Center, Lightformer, Billboard, Sparkles, Shadow, Sky } from "@react-three/drei";
 import { Model } from './Fuku-san';
-import { ReactNode, useRef } from 'react';
+import { ReactNode, useRef, useState } from 'react';
 import { easing } from 'maath'
 
-export const Fuku3D = ({ position = [0, 0, 2.5], fov = 25 }) => {
+export const Fuku3D = ({ position = [0, 0, 2.5], fov = 25 }: any) => {
+  const [perfSucks, degrade] = useState(true)
+
   return (
     <Canvas
       shadows
       camera={{ position, fov }}
       gl={{ preserveDrawingBuffer: true }}
-      //eventSource={document.getElementById('root')}
       eventPrefix="client"
       //className='touch-none w-full h-full m-0 p-0 overflow-hidden user-select-none'
     >
-      <ambientLight intensity={0.5} />
-      <Environment files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/potsdamer_platz_1k.hdr" />
-
+      <Env perfSucks={perfSucks} />
       <CameraRig>
         {/*<Backdrop />*/}
         <Center>
           <Model scale={0.3} />
+          <Shadow rotation={[-Math.PI / 2, 0, 0]} scale={0.3} position={[0, -0.3, 0]} color={"black"} opacity={0.5} />
+          <Sparkles count={20} scale={0.3 * 2} size={6} speed={0.4} />
         </Center>
       </CameraRig>
+      <Sky distance={450000} sunPosition={[0, 1, 0.8]} inclination={0} azimuth={0.25} />
     </Canvas>
-  )
-  /*return (
-    <div className='h-[80vh]'>
-      <Canvas shadows>
-        <Stage environment="city" intensity={0.6}>
-          <Model name="huku-san" scale={1} />
-          <Model name="HMD" scale={1} />
-          <Caustics
-            backfaces
-            color={[1, 0.8, 0.8]}
-            focus={[0, -1.2, 0]}
-            lightSource={[-2, 2.5, -2.5]}
-            frustum={1.75}
-            intensity={0.005}
-            worldRadius={0.66 / 10}
-            ior={0.6}
-            backfaceIor={1.26}>
-            <mesh castShadow receiveShadow geometry={nodes.glass.geometry}>
-              <MeshTransmissionMaterial thickness={0.2} chromaticAberration={0.05} anisotropy={1.5} clearcoat={1} clearcoatRoughness={0.2} envMapIntensity={3} />
-            </mesh>
-          </Caustics>
-        </Stage>
-        <BakeShadows />
-        <OrbitControls makeDefault autoRotate />
-      </Canvas>
-    </div>
-  )*/
-}
-
-function Backdrop() {
-  const shadows = useRef()
-  useFrame((state, delta) => easing.dampC(shadows.current.getMesh().material.color, state.color, 0.25, delta))
-  return (
-    <AccumulativeShadows ref={shadows} temporal frames={60} alphaTest={0.85} scale={10} rotation={[Math.PI / 2, 0, 0]} position={[0, 0, -0.14]}>
-      <RandomizedLight amount={4} radius={9} intensity={0.55} ambient={0.25} position={[5, 5, -10]} />
-      <RandomizedLight amount={4} radius={5} intensity={0.25} ambient={0.55} position={[-5, 5, -9]} />
-    </AccumulativeShadows>
   )
 }
 
@@ -78,4 +43,28 @@ function CameraRig({ children }: RigType) {
     easing.dampE(group.current.rotation, [state.pointer.y / 10, -state.pointer.x / 5, 0], 0.25, delta)
   })
   return <group ref={group}>{children}</group>
+}
+
+type EnvProps = {
+  perfSucks: boolean
+}
+
+function Env({ perfSucks }: EnvProps) {
+  return (
+    <Environment frames={perfSucks ? 1 : Infinity} preset="sunset" resolution={256} background blur={0.8}>
+      <Lightformer intensity={4} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
+      <Lightformer intensity={4} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
+      <group rotation={[Math.PI / 2, 1, 0]}>
+        {[2, -2, 2, -4, 2, -5, 2, -9].map((x, i) => (
+          <Lightformer key={i} intensity={1} rotation={[Math.PI / 4, 0, 0]} position={[x, 4, i * 4]} scale={[4, 1, 1]} />
+        ))}
+        <Lightformer intensity={0.5} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={[50, 2, 1]} />
+        <Lightformer intensity={0.5} rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={[50, 2, 1]} />
+        <Lightformer intensity={0.5} rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={[50, 2, 1]} />
+        </group>
+      <group>
+        <Lightformer intensity={5} form="ring" color="silver" rotation-y={Math.PI / 2} position={[-5, 2, -1]} scale={[10, 10, 1]} />
+      </group>
+    </Environment>
+  )
 }
