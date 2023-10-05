eacf66c71ea26bb973078c358d4c5237f400bebc
nishida <42875682+nishidayoshikatsu@users.noreply.github.com>
2023-10-05 19:10:53 +0900

    :hammer: Add status and link badge

--- src/components/events/EventsList.tsx
+++ src/components/events/EventsList.tsx
@@ -1,13 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from 'next/image'
-
-import { EventsLinks } from '@/data/events'
 import { getDatabase } from '@/lib/notion'
 import ExternalLink from '../share/externalLink'
-
-type Props = {
-  all: boolean
-}
+import { ExternalLink as ExternalLinkIcon } from 'lucide-react'
+import { Button } from '../ui/button'
 
 const eventDatabaseId = process.env.NOTION_EVENT_DATABASE_ID ?? ''
 
@@ -23,7 +18,7 @@ export const EventsList = async () => {
 
   //TODO: ページネーションを実装する
   //将来のイベント取得
-  const featureEvents = await getDatabase({
+  /*const featureEvents = await getDatabase({
     databaseId: eventDatabaseId,
     filter: {
       property: 'イベント日',
@@ -38,7 +33,7 @@ export const EventsList = async () => {
       },
     ],
     page_size: 100,
-  })
+  })*/
 
   //直近のイベント3件を取得
   /*const recentPastEvents = await getDatabase({
@@ -77,12 +72,11 @@ export const EventsList = async () => {
           const eventDate = event.properties['イベント日'].date.start
           const eventTitle = event.properties['名前'].title[0].text.content
           const eventDescription = event.properties['概要'].rich_text.length > 0 ? event.properties['概要'].rich_text[0].plain_text : ''
-          const eventImageUrl = event.cover ? event.cover.file.url : ''
+          const eventImageUrl = event.cover ? (event.cover.file ? event.cover.file.url : event.cover.external.url) : ''
           const connpassUrl = event.properties.connpass ? event.properties.connpass.url : ''
           const peatixUrl = event.properties.peatix ? event.properties.peatix.url : ''
           const clusterUrl = event.properties.cluster ? event.properties.cluster.url : ''
           const youtubeUrl = event.properties.youtube ? event.properties.youtube.url : ''
-
           return (
             <li
               key={event.id}
@@ -103,12 +97,51 @@ export const EventsList = async () => {
                   <time dateTime={eventDate} className="text-gray-500">
                     {eventDate}
                   </time>
-                  <ExternalLink
-                    href={connpassUrl}
-                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                  >
-                    {"connpass"}
-                  </ExternalLink>
+                  {eventDate > formattedDate &&
+                    <span className="inline-flex items-center gap-x-1.5 rounded-md bg-yellow-100 px-2 py-1 text-xs font-medium text-yellow-800">
+                      <svg className="h-1.5 w-1.5 fill-yellow-500" viewBox="0 0 6 6" aria-hidden="true">
+                        <circle cx={3} cy={3} r={3} />
+                      </svg>
+                      参加者募集中
+                    </span>
+                  }
+                </div>
+                <div className='flex space-x-1 mt-4'>
+                  {youtubeUrl &&
+                    <ExternalLink
+                      href={youtubeUrl}
+                      className="inline-flex z-10 items-center gap-x-1.5 rounded-full bg-[#FF0000] px-3 py-1.5 text-xs font-medium text-white hover:bg-[#FF0000]/50">
+                      YouTube
+                      <ExternalLinkIcon className="h-4 w-4" />
+                    </ExternalLink>
+                  }
+                  {connpassUrl &&
+                    <ExternalLink
+                      href={connpassUrl}
+                      className="inline-flex z-10 items-center gap-x-1.5 rounded-full bg-[#B22501] px-3 py-1.5 text-xs font-medium text-white hover:bg-[#B22501]/50"
+                    >
+                      connpass
+                      <ExternalLinkIcon className="h-4 w-4" />
+                    </ExternalLink>
+                  }
+                  {peatixUrl &&
+                    <ExternalLink
+                      href={peatixUrl}
+                      className="inline-flex z-10 items-center gap-x-1.5 rounded-full bg-[#56AB48] px-3 py-1.5 text-xs font-medium text-white hover:bg-[#56AB48]/50"
+                    >
+                      Peatix
+                      <ExternalLinkIcon className="h-4 w-4" />
+                    </ExternalLink>
+                  }
+                  {clusterUrl &&
+                    <ExternalLink
+                      href={clusterUrl}
+                      className="inline-flex z-10 items-center gap-x-1.5 rounded-full bg-[#00a6ea] px-3 py-1.5 text-xs font-medium text-white hover:bg-[#00a6ea]/50"
+                    >
+                      Cluster
+                      <ExternalLinkIcon className="h-4 w-4" />
+                    </ExternalLink>
+                  }
                 </div>
                 <div className="group relative">
                   <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
@@ -126,39 +159,4 @@ export const EventsList = async () => {
       </ul>
     </div>
   )
-
-  /*return (
-    <>
-      {!all && !mkdata(all, events).length
-        ? (
-        <div className="flex justify-center my-10">現在開催予定のイベントはありません。</div>
-          )
-        : (
-        <ul className="flex mt-10 justify-center flex-col　md:flex-row flex-wrap max-w-7xl mx-auto px-4 sm:px-6">
-          {mkdata(all, events).map((event: anyt) => (
-            <li
-              key={event.title}
-              className="flex flex-col justify-start items-center bg-white mb-10 mx-6 md:w-2/5"
-            >
-              <a href={event.url} className="px-10">
-                <Image
-                  className="flex justify-center"
-                  src={event.image}
-                  alt={event.title}
-                  //layout="fixed"
-                  width={300}
-                  height={150}
-                />
-
-                <h3 className="mt-6 mb-4 text-normal-hover text-2xl font-medium hover:text-primary max-w-sm">
-                  {event.title}
-                </h3>
-                <div className="text-normal-default text-sm">{event.date}</div>
-              </a>
-            </li>
-          ))}
-        </ul>
-          )}
-    </>
-  )*/
 }
