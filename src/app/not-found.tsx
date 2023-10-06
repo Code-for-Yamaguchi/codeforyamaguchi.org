9eba5e432ea3a7eb022368ce9a3a43566ed059df
nishida <42875682+nishidayoshikatsu@users.noreply.github.com>
2023-10-06 10:59:48 +0900

    :lipstick: Adjust style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <main className="mx-auto flex w-full max-w-7xl flex-auto flex-col justify-center px-6 py-24 sm:py-64 lg:px-8">
-      <p className="text-base font-semibold leading-8 text-indigo-600">404</p>
+      <p className="text-base font-semibold leading-8 text-[#3CB840]">404</p>
       <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
         お探しのページは見つかりませんでした。
       </h1>
@@ -13,7 +13,7 @@ export default function NotFound() {
       <div className="mt-10">
         <Link
           href="/"
-          className="text-sm font-semibold leading-7 text-indigo-600"
+          className="text-sm font-semibold leading-7 text-[#3CB840]"
         >
           <span aria-hidden="true">&larr;</span> TOPページへ戻る
         </Link>
